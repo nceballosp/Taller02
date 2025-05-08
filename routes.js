21ec0a6b8355a79be61a661e88f2222690a61c44
jjvasquezg <juanjos137k@gmail.com>
2025-05-07 21:31:23 -0500

    Actualización de imagenes y contenedor ID

--- routes.js
+++ routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const pokeneas = require('./util/pokeneas');
+const os = require("os");
 
 
 router.get('/', (req, res) => {
     const number = Math.floor(Math.random() * pokeneas.length);
     const { phrase, ...viewData } = pokeneas[number];
+    viewData.containerId = os.hostname();
     res.render('index', { pokenea: viewData });
   })
 
 router.get('/phrase', (req, res) => {
     const number = Math.floor(Math.random() * pokeneas.length);
     const viewData = pokeneas[number];
+    viewData.containerId = os.hostname();
     res.render('phrase', { pokenea: viewData});
 })
 module.exports = router;
